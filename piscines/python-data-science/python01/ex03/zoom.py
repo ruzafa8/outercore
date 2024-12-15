8d8f028f263308fca3c154e4b2862675bccba887
Alejandro jose Ruzafa casas <aruzafa-@c1r14s1.42malaga.com>
2024-12-15 19:44:20 +0100

    fix(python-data-science): improve zoom exercite

--- piscines/python-data-science/python01/ex03/zoom.py
+++ piscines/python-data-science/python01/ex03/zoom.py
@@ -2,8 +2,16 @@ from load_image import ft_load
 import cv2
 
 
-def zoom(img, zoom_factor=2):
-    return cv2.resize(img, None, fx=zoom_factor, fy=zoom_factor, interpolation=0)
+def resize(img, window_size, at):
+    
+    margin = [
+        [
+            y + x for y in [-round(window_size/2), round(window_size/2)]
+        ] for x in at
+    ]
+    x_slice = slice(margin[0][0], margin[0][1])
+    y_slice = slice(margin[1][0], margin[1][1])
+    return img[x_slice, y_slice, :1]
 
 
 def print_img(img):
@@ -13,10 +21,12 @@ def print_img(img):
 
 def main():
     img = ft_load("animal.jpeg")
-    print_img(img)
-    img_zoomed = zoom(img)
-    print_img(img_zoomed)
     print(img)
+    print_img(img)
+    img_zoomed = resize(img, 400, [300, 650])
+    print(f"New shape after slicing: {img_zoomed.shape}")
+    print(img_zoomed)
+    print_img(img_zoomed)
 
 
 if __name__ == "__main__":
