7c0650454bfc5379dd266ccd55f76bc1a85124a2
Alejandro jose Ruzafa casas <aruzafa-@c1r14s1.42malaga.com>
2024-12-15 12:30:05 +0100

    fix(python-data-science): python00-ex03 remove files and fix warning

--- piscines/python-data-science/python00/ex03/NULL_not_found.py
+++ piscines/python-data-science/python00/ex03/NULL_not_found.py
@@ -3,12 +3,12 @@ def NULL_not_found(object: any) -> int:
         print(f"Nothing: {object} {type(object)}")
     elif object != object:
         print(f"Cheese: {object} {type(object)}")
-    elif object is 0:
+    elif object is False:
+        print(f"Fake: {object} {type(object)}")
+    elif object == 0:
         print(f"Zero: {object} {type(object)}")
     elif object == '':
         print(f"Empty: {object} {type(object)}")
-    elif object is False:
-        print(f"Fake: {object} {type(object)}")
     else:
         print("Type not found")
         return 1
