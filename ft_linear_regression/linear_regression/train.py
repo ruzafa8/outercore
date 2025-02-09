2d9a430d5732a7e31c6f96d92df068607c5a6e03
Alejandro jose Ruzafa casas <aruzafa-@c3r1s1.42malaga.com>
2025-02-09 19:05:26 +0100

    feat(ft_linear_regression): add bonus

--- ft_linear_regression/linear_regression/train.py
+++ ft_linear_regression/linear_regression/train.py
@@ -1,4 +1,5 @@
 from linear_regression.load_csv import load
+import matplotlib.pyplot as plt
 
 
 class LinearRegression:
@@ -10,6 +11,7 @@ class LinearRegression:
         regression = LinearRegression()
         data = load(file)
         regression.data = data
+        regression.original_data = data.copy()
         regression._normalize_data()
 
         regression.theta0 = 0
@@ -24,7 +26,7 @@ class LinearRegression:
         regression.theta1 = theta1
         return regression
 
-    def train(self, learning_rate: float, iterations: int):
+    def train(self, learning_rate: float, iterations: int, debug: bool):
         try:
             for i in range(iterations):
                 acc_theta0: float = 0.0
@@ -33,7 +35,8 @@ class LinearRegression:
                     error = self.estimate_price(row.km) - row.price
                     acc_theta0 = acc_theta0 + error
                     acc_theta1 = acc_theta1 + error * row.km
-                if i % 1000 == 0:
+                if debug and i % 1000 == 0:
+                    pass
                     print(f"Iter {i}: θ0={self.theta0}, θ1={self.theta1},\
                           Error Prom={acc_theta0/self.m}")
                 self.theta0 = self.theta0 - learning_rate * acc_theta0 / self.m
@@ -42,6 +45,32 @@ class LinearRegression:
 
         except BaseException as e:
             print(f"{type(e).__name__}: {e}")
+
+    def plot_data(self):
+        data = self.original_data
+
+        plt.plot(data.km, data.price, 'o')
+        plt.axline((0, self.theta0), slope=self.theta1, color='g')
+
+        plt.title('kms/price')
+        plt.xlabel('kms')
+        plt.ylabel('price')
+
+        plt.show()
+
+    def get_accuracy(self):
+        pred = [self.estimate_price(km) for km in self.original_data.km]
+        RSS = sum((self.original_data.price - pred) ** 2)
+        TSS = sum((self.original_data.price - self.original_data.price.mean()) ** 2)
+        R2 = 1 - RSS / TSS
+
+        MAE = sum(abs(self.original_data.price - pred)) / self.m
+        RMSE = (RSS / self.m) ** 0.5
+        return {
+            "R2": R2,      # (0, 1)
+            "MAE": MAE,    # mean of errors
+            "RMSE": RMSE,  # espected difference between prediction and real
+        }
 
     def _normalize_data(self):
         min_data = min(self.data["km"])
--- ft_linear_regression/train.py
+++ ft_linear_regression/train.py
@@ -3,22 +3,43 @@ from linear_regression.train import LinearRegression
 
 
 def linear_regression_train():
+    args = parse_params()
+
+    regression = LinearRegression.load_file(args.file)
+    regression.train(learning_rate=args.learning_rate,
+                     iterations=args.iterations, debug=args.debug)
+
+    print("============================================")
+    print("Training result:")
+    print("θ₀ = ", regression.theta0)
+    print("θ₁ = ", regression.theta1)
+    print("============================================")
+    if args.accuracy:
+        print("Accuracy measurement:")
+        print(regression.get_accuracy())
+        print("============================================")
+
+    if args.plot:
+        regression.plot_data()
+
+    with open(args.output, "w") as file:
+        file.write(f"{regression.theta0}:{regression.theta1}")
+
+
+def parse_params():
     parser = argparse.ArgumentParser(description="Linear regression project.")
     parser.add_argument("file", help='file name of the csv data', nargs="?",
                         default='data.csv')
     parser.add_argument("-l", "--learning-rate", type=float, default=0.01)
     parser.add_argument("-i", "--iterations", type=int, default=10000)
     parser.add_argument("-o", "--output", type=str, default="theta.data")
-    args = parser.parse_args()
-
-    regression = LinearRegression.load_file(args.file)
-    regression.train(learning_rate=args.learning_rate,
-                     iterations=args.iterations)
-
-    print("Theta0: ", regression.theta0)
-    print("Theta1: ", regression.theta1)
-    with open(args.output, "w") as file:
-        file.write(f"{regression.theta0}:{regression.theta1}")
+    parser.add_argument("-p", "--plot", action="store_true", default=False,
+                        help="plot result of training in a graphic.")
+    parser.add_argument("-d", "--debug", action="store_true", default=False,
+                        help="show debug training info.")
+    parser.add_argument("-a", "--accuracy", action="store_true", default=False,
+                        help="show model accuracy.")
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
